75f137e857518e30df3b540325af0350930fd49e
Rafael Lucena <rafellk@gmail.com>
2019-11-12 23:38:13 -0300

    Create characters controller and collection view
    This patch was added to create the characters view controller link to
    the characters storyboard and characters collection view that will
    handle the cells presentation itself.

---
+++ MarvelApp/MarvelApp/Features/Characters/Views/CharactersCollectionView/CharactersCollectionView.swift
@@ -0,0 +1,23 @@
+//
+//  CharactersCollectionView.swift
+//  MarvelApp
+//
+//  Created by Rafael Lucena on 11/12/19.
+//  Copyright © 2019 RLMG. All rights reserved.
+//
+
+import UIKit
+
+class CharactersCollectionView: UICollectionView {
+}
+
+extension CharactersCollectionView: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
---
+++ MarvelApp/MarvelApp/Features/Characters/Views/CharactersViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CharactersViewController.swift
+//  MarvelApp
+//
+//  Created by Rafael Lucena on 11/12/19.
+//  Copyright © 2019 RLMG. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: CharactersCollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationItem()
+    }
+    
+    private func configureNavigationItem() {
+        // TODO: localize this
+        navigationItem.title = "Characters"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
